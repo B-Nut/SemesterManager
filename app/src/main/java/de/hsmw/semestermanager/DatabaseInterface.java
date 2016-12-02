de662e70dadd241b595a660a7ff0f0cbb65a8b0a
Adrian <ortenburger.adrian@gmail.com>
2016-12-02 19:04:55 +0100

    Updatefunktion hinzugefügt; zwei, drei kleine Schreibfehler behoben

--- app/src/main/java/de/hsmw/semestermanager/DatabaseInterface.java
+++ app/src/main/java/de/hsmw/semestermanager/DatabaseInterface.java
@@ -1,68 +1,48 @@
 package de.hsmw.semestermanager;
 
 import android.content.ContentValues;
+import android.content.Entity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.util.Log;
 
 /**
- * Created by Adrian on 26.11.2016.
+ * Created by Benni on 26.11.2016.
  */
 
-/**
- --------------------- Query-Liste------------------------------------
-add plans(old)      = insertDataPlans(String name, String timeframe)
-add plans           = insertDataPlans(String ANZEIGENAME, String ZEITRAUM)
-add modules         = insertDataModules(String ANZEIGENAME, int SEMESTERID)
-add entries         = insertDataEntries(String ANZEIGENAME, String ZEITRAUM,String ORT, String TERMINTYP,int PRIORITAET, int SEMESTERID, int MODULID, int ISTGANZTAGSTERMIN, String DOZENT)
-
-get all from plans(old)                       = getAllData()
-get all from plans; ORDER BY ANZEIGENAME      = getAllDataPlans()
-get all from modules; ORDER BY ANZEIGENAME    = getAllDataModules()
-get all from entries; ORDER BY ANZEIGENAME    = getAllDataEntries()
-
-get row by plans.id(old) = getDataByID(int id)
-get row by plans.id      = getDataByIDPlans(int id)
-get row by moduls.id     = getDataByIDModules(int id)
-get row by Entries.id    = getDataByIDEntries(int id)
-
-search after string; order by zeitraum    = getListByStringPlans(int id)
-search after string; order by name        = getListByStringModules(int id)
-search after string; order by zeitraum    = getListByStringEntries(int id)
-
-
-
- */
 
 public class DatabaseInterface {
     private SQLiteDatabase db;
 
-    DatabaseInterface(SQLiteDatabase db){
+    DatabaseInterface(SQLiteDatabase db) {
         this.db = db;
     }
 
 
-    public long insertData(String name, String timeframe){
+    public long insertData(String name, String timeframe) {
         ContentValues values = new ContentValues();
         values.put("Anzeigename", name);
         values.put("zeitraum", timeframe);
         return db.insert("plans", null, values);
     }
+
     //---------------------------Add Data----------------------------------------------
-    public long insertDataPlans(String ANZEIGENAME, String ZEITRAUM){
+    public long insertDataPlans(String ANZEIGENAME, String ZEITRAUM) {
         ContentValues values = new ContentValues();
         values.put("ANZEIGENAME", ANZEIGENAME);
         values.put("ZEITRAUM", ZEITRAUM);
         return db.insert("plans", null, values);
     }
-    public long insertDataModules(String ANZEIGENAME, int SEMESTERID){
+
+    public long insertDataModules(String ANZEIGENAME, int SEMESTERID) {
         ContentValues values = new ContentValues();
         values.put("ANZEIGENAME", ANZEIGENAME);
         values.put("SEMESTERID", SEMESTERID);
-        return db.insert("modules", null, values);}
+        return db.insert("modules", null, values);
+    }
 
-    public long insertDataEntries(String ANZEIGENAME, String ZEITRAUM,String ORT, String TERMINTYP,int PRIORITAET, int SEMESTERID, int MODULID, int ISTGANZTAGSTERMIN, String DOZENT){
+    public long insertDataEntries(String ANZEIGENAME, String ZEITRAUM, String ORT, String TERMINTYP, int PRIORITAET, int SEMESTERID, int MODULID, int ISTGANZTAGSTERMIN, String DOZENT) {
         ContentValues values = new ContentValues();
         values.put("ANZEIGENAME", ANZEIGENAME);
         values.put("ZEITRAUM", ZEITRAUM);
@@ -77,52 +57,95 @@ public class DatabaseInterface {
     }
     //------------------------------Get All Data-----------------------------------------
 
-    public Cursor getAllData(){
-        return db.rawQuery("select * from plans",null);
+    public Cursor getAllData() {
+        return db.rawQuery("select * from plans", null);
     }
-    public Cursor getAllDataPlans(){
-        return db.rawQuery("select * from plans ORDER BY ANZEIGENAME",null);
+
+    public Cursor getAllDataPlans() {
+        return db.rawQuery("select * from plans ORDER BY ZEITRAUM", null);
     }
-    public Cursor getAllDataModules(){
-        return db.rawQuery("select * from modules ORDER BY ANZEIGENAME",null);
+
+    public Cursor getAllDataModules() {
+        return db.rawQuery("select * from modules ORDER BY ANZEIGENAME", null);
     }
-    public Cursor getAllDataEntries(){
-        return db.rawQuery("select * from entries ORDER BY ANZEIGENAME",null);
+
+    public Cursor getAllDataEntries() {
+        return db.rawQuery("select * from entries ORDER BY ZEITRAUM", null);
     }
 
     //----------------------------Get Data By ID---------------------------
-    public Cursor getDataByID(int id){
+    public Cursor getDataByID(int id) {
         String[] columns = {"*"};
-        String query = SQLiteQueryBuilder.buildQueryString(false,"plans",columns,"ID = " + id,"","","","");
+        String query = SQLiteQueryBuilder.buildQueryString(false, "plans", columns, "ID = " + id, "", "", "", "");
         Log.d("database", query);
-        return db.rawQuery(query,null);
-    }
-    public Cursor getDataByIDPlans(int id){
-        String[] columns = {"*"};
-        String query = SQLiteQueryBuilder.buildQueryString(false,"plans",columns,"ID = " + id,"","","","");
-        Log.d("database", query);
-        return db.rawQuery(query,null);
-    }
-    public Cursor getDataByIDModules(int id){
-        String[] columns = {"*"};
-        String query = SQLiteQueryBuilder.buildQueryString(false,"modules",columns,"ID = " + id,"","","","");
-        Log.d("database", query);
-        return db.rawQuery(query,null);
-    }
-    public Cursor getDataByIDEntries(int id){
-        String[] columns = {"*"};
-        String query = SQLiteQueryBuilder.buildQueryString(false,"entries",columns,"ID = " + id,"","","","");
-        Log.d("database", query);
-        return db.rawQuery(query,null);
+        return db.rawQuery(query, null);
     }
 
-    //-------------------------suche nach ---------------------------------------------
+    public Cursor getDataByIDPlans(int id) {
+        String[] columns = {"*"};
+        String query = SQLiteQueryBuilder.buildQueryString(false, "plans", columns, "ID = " + id, "", "", "", "");
+        Log.d("database", query);
+        return db.rawQuery(query, null);
+    }
 
-    public Cursor getListByStringPlans(int id){return db.rawQuery("SELECT * FROM plans ORDER BY ZEITRAUM",null);}
-    public Cursor getListByStringModules(int id){return db.rawQuery("SELECT * FROM modules ORDER BY ANZEIGENAME",null);}
-    public Cursor getListByStringEntries(int id){return db.rawQuery("SELECT * FROM entries ORDER BY ZEITRAUM",null);}
+    public Cursor getDataByIDModules(int id) {
+        String[] columns = {"*"};
+        String query = SQLiteQueryBuilder.buildQueryString(false, "modules", columns, "ID = " + id, "", "", "", "");
+        Log.d("database", query);
+        return db.rawQuery(query, null);
+    }
 
+    public Cursor getDataByIDEntries(int id) {
+        String[] columns = {"*"};
+        String query = SQLiteQueryBuilder.buildQueryString(false, "entries", columns, "ID = " + id, "", "", "", "");
+        Log.d("database", query);
+        return db.rawQuery(query, null);
 
+    }
 
+    //-------------------------searching for---------------------------------------------
 
+    public Cursor getListByStringPlans(String searchword) {
+        return db.rawQuery("SELECT * FROM plans WHERE ANZEIGENAME LIKE \"%" + searchword + "%\"ORDER BY ZEITRAUM", null);
+    }
+
+    public Cursor getListByStringModules(String searchword) {
+        return db.rawQuery("SELECT * FROM modules WHERE ANZEIGENAME LIKE \"%" + searchword + "%\"ORDER BY ANZEIGENAME", null);
+    }
+
+    public Cursor getListByStringEntries(String searchword) {
+        return db.rawQuery("SELECT * FROM entries WHERE ANZEIGENAME LIKE \"%" + searchword + "%\"ORDER BY ZEITRAUM", null);
+    }
+    //----------------------Update------------------------------------------------------
+
+    //UPDATE Customers SET CustomerName='Hamburg', ContactName='Hamburg',Address='Hamburg' WHERE CustomerID=1;
+
+    public long updateDataPlans(int ID, String ANZEIGENAME, String ZEITRAUM) {
+        ContentValues values = new ContentValues();
+        values.put("ANZEIGENAME", ANZEIGENAME);
+        values.put("ZEITRAUM", ZEITRAUM);
+        return db.update("plans", values, "WHERE ID =" + String.valueOf(ID), null);
+
+    }
+
+    public long updateDataModules(int ID, String ANZEIGENAME, int SEMESTERID) {
+        ContentValues values = new ContentValues();
+        values.put("ANZEIGENAME", ANZEIGENAME);
+        values.put("SEMESTERID", SEMESTERID);
+        return db.update("Modules", values, "WHERE ID =" + String.valueOf(ID), null);
+    }
+
+    public long updateDataEntries(int ID, String ANZEIGENAME, String ZEITRAUM, String ORT, String TERMINTYP, int PRIORITAET, int SEMESTERID, int MODULID, int ISTGANZTAGSTERMIN, String DOZENT) {
+        ContentValues values = new ContentValues();
+        values.put("ANZEIGENAME", ANZEIGENAME);
+        values.put("ZEITRAUM", ZEITRAUM);
+        values.put("ORT", ORT);
+        values.put("TERMINTYP", TERMINTYP);
+        values.put("PRIORITAET", PRIORITAET);
+        values.put("SEMESTERID", SEMESTERID);
+        values.put("MODULID", MODULID);
+        values.put("ISTGANZTAGSTERMIN", ISTGANZTAGSTERMIN);
+        values.put("DOZENT", DOZENT);
+        return db.update("Entries", values, "WHERE ID =" + String.valueOf(ID), null);
+    }
 }
