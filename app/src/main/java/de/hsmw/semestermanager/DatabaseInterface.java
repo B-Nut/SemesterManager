4b9bdd1ea2ea807367c9f9169d12c9fe1309c789
Adrian <ortenburger.adrian@gmail.com>
2016-12-04 17:25:56 +0100

    DatabaseInterface und DatabaseHandler, erweiter, ueberarbeitet und verbessert

--- app/src/main/java/de/hsmw/semestermanager/DatabaseInterface.java
+++ app/src/main/java/de/hsmw/semestermanager/DatabaseInterface.java
@@ -31,20 +31,12 @@ public class DatabaseInterface {
         values.put("ENDTIME", ENDTIME);
         return db.insert("plans", null, values);
     }
-
     public long insertDataModules(String ANZEIGENAME, int SEMESTERID) {
         ContentValues values = new ContentValues();
         values.put("ANZEIGENAME", ANZEIGENAME);
         values.put("SEMESTERID", SEMESTERID);
         return db.insert("modules", null, values);
     }
-  /*  public long insertDataRevision(String STARTDATE,String ENDDATA, int PRIORITAET) {
-        ContentValues values = new ContentValues();
-        values.put("STARTDATE", STARTDATE);
-        values.put("ENDDATA", ENDDATA);
-        values.put("PRIORITAET", PRIORITAET);
-        return db.insert("modules", null, values);
-    }*/
     public long insertDataRevisionException(int PRIORITAET,int ISDELETED,String STARTDATE,String ENDDATA,String STARTTIME,String ENDTIME) {
         ContentValues values = new ContentValues();
         values.put("PRIORITAET", PRIORITAET);
@@ -91,17 +83,6 @@ public class DatabaseInterface {
         }
         return returnModule;
     }
-
-
-
-    public Plan[] getAllRevisionException(){
-        Cursor c = db.rawQuery("select * from revisionException ORDER BY STARTTIME, STARTTIME, ENDDATA, ENDTIME", null);
-        Plan[] returnPlans = new Plan[c.getCount()];
-        while (c.moveToNext()) {
-            returnPlans[c.getPosition()] = new Plan(c.getInt(0), c.getString(1), c.getString(2), c.getString(3));
-        }
-        return returnPlans;
-    }
     public Entry[] getAllDataEntries(){
         Cursor c = db.rawQuery("select * from entries ORDER BY STARTTIME, STARTTIME, ENDDATA, ENDTIME ", null);
         Entry[] returnEntry = new Entry[c.getCount()];
@@ -130,7 +111,6 @@ public class DatabaseInterface {
         Module p = new Module(c.getInt(0), c.getString(1), c.getInt(2));
         c.close();
         return  p;
-
     }
     public Entry getDataByIDEntries(int id) {
         String[] columns = {"*"};
@@ -142,19 +122,6 @@ public class DatabaseInterface {
         c.close();
         return  p;
     }
-
-    public Module[] getModulesByPlanID(int id){
-        String[] columns = {"*"};
-        String query = SQLiteQueryBuilder.buildQueryString(false, "modules", columns, "SEMESTERID = " + id, "", "", "", "");
-        Cursor c = db.rawQuery(query,null);
-        Module[] returnArray = new Module[c.getCount()];
-        while (c.moveToNext()){
-            returnArray[c.getPosition()] = new Module(c.getInt(0), c.getString(1), c.getInt(2));
-        }
-        c.close();
-        return returnArray;
-    }
-
     public Entry[] getEntriesByPlanID(int id){
         String[] columns = {"*"};
         String query = SQLiteQueryBuilder.buildQueryString(false, "entries", columns, "SEMESTERID = " + id, "", "", "", "");
@@ -166,8 +133,29 @@ public class DatabaseInterface {
         c.close();
         return returnArray;
     }
-
-
+    public Entry[] getEntriesByModulID(int id){
+        String[] columns = {"*"};
+        String query = SQLiteQueryBuilder.buildQueryString(false, "entries", columns, "ModulID = " + id, "", "", "", "");
+        Cursor c = db.rawQuery(query,null);
+        Entry[] returnArray = new Entry[c.getCount()];
+        while (c.moveToNext()){
+            returnArray[c.getPosition()] = new Entry(c.getInt(0),c.getString(1),c.getString(2),c.getString(3),c.getString(4),c.getString(5),c.getString(6),c.getString(7),c.getString(8),c.getString(9),c.getInt(10),c.getInt(11),c.getInt(12),c.getInt(13),c.getString(14),c.getInt(15));
+        }
+        c.close();
+        return returnArray;
+    }
+    public Entry[] getEntriesByPlanIDUndNichtModul(int id){
+        String[] columns = {"*"};
+        String query = "select * from entries WHERE SEMESTERID =  \""+ id +  "\" AND ModulID = \"0\" ORDER BY STARTTIME, ENDTIME,";
+        //String query = SQLiteQueryBuilder.buildQueryString(false, "entries", columns, "SEMESTERID = " + id AND "ModulID = " + 0, "", "", "", "");
+        Cursor c = db.rawQuery(query,null);
+        Entry[] returnArray = new Entry[c.getCount()];
+        while (c.moveToNext()){
+            returnArray[c.getPosition()] = new Entry(c.getInt(0),c.getString(1),c.getString(2),c.getString(3),c.getString(4),c.getString(5),c.getString(6),c.getString(7),c.getString(8),c.getString(9),c.getInt(10),c.getInt(11),c.getInt(12),c.getInt(13),c.getString(14),c.getInt(15));
+        }
+        c.close();
+        return returnArray;
+    }
 //-------------------------searching for---------------------------------------------
     public Plan[] getListByStringPlans(String searchword) {
         Cursor c = db.rawQuery("SELECT * FROM plans WHERE ANZEIGENAME LIKE \"%" + searchword + "%\"ORDER BY ZEITRAUM", null);
@@ -178,7 +166,6 @@ public class DatabaseInterface {
         c.close();
         return returnPlan;
     }
-
     public Module[] getListByStringModules(String searchword) {
         Cursor c = db.rawQuery("SELECT * FROM modules WHERE ANZEIGENAME LIKE \"%" + searchword + "%\"ORDER BY ZEITRAUM", null);
         Module[] returnModules = new Module[c.getCount()];
@@ -197,7 +184,6 @@ public class DatabaseInterface {
         c.close();
         return returnEntries;
     }
-
 //----------------------Update------------------------------------------------------
     public long updateDataPlans(int ID, String ANZEIGENAME, String STARTTIME, String ENDTIME) {
         ContentValues values = new ContentValues();
@@ -205,16 +191,13 @@ public class DatabaseInterface {
         values.put("STARTTIME", STARTTIME);
         values.put("ENDTIME", ENDTIME);
         return db.update("plans", values, "WHERE ID =" + String.valueOf(ID), null);
-
     }
-
     public long updateDataModules(int ID, String ANZEIGENAME, int SEMESTERID) {
         ContentValues values = new ContentValues();
         values.put("ANZEIGENAME", ANZEIGENAME);
         values.put("SEMESTERID", SEMESTERID);
         return db.update("Modules", values, "WHERE ID =" + String.valueOf(ID), null);
     }
-
     public long updateDataEntries(int id, String name, String startDate, String endDate, String wiederholungsStart, String wiederholungsEnde, String startTime, String endTime, String ort, String typ, int prioritaet, int planID, int modulID, int istGanztagsTermin, String dozent, int periode) {
         ContentValues values = new ContentValues();
         values.put("ANZEIGENAME", name);
@@ -235,7 +218,4 @@ public class DatabaseInterface {
         return db.update("Entries", values, "WHERE ID =" + String.valueOf(id), null);
     }
     //------------------------------------------------------------------
-   /* semid = list aller module
-    modul id = liste aller entis
-    semid = alle entis die nicht zu einem modul gehoren*/
 }
