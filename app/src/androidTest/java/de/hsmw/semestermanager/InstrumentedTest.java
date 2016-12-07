0c0e940e3eb85fb56c5ddf233b39ce4cba6ad511
matthias <matthiaskoszinski@gmx.de>
2016-12-07 23:47:23 +0100

    erster sinnvoller Test

--- app/src/androidTest/java/de/hsmw/semestermanager/InstrumentedTest.java
+++ app/src/androidTest/java/de/hsmw/semestermanager/InstrumentedTest.java
@@ -19,11 +19,16 @@ public class InstrumentedTest {
     @Test
     public void test() throws Exception {
         // Context of the app under test.
+        String name = "Test";
+        String startDate = "2016-05-01";
+        String endDate = "2016-12-30";
         Context appContext = InstrumentationRegistry.getTargetContext();
         DatabaseHandler dh = new DatabaseHandler(appContext);
         DatabaseInterface di = new DatabaseInterface(dh.getWritableDatabase());
-        int foobar = (int) di.insertDataPlans("Test", "2016.05.01", "2016.12.30");
-
-        assertEquals(1, foobar);
+        int foobar = (int) di.insertDataPlans(name, startDate, endDate);
+        Plan plan = di.getDataByIDPlans(foobar);
+        assertEquals(name, plan.getName());
+        assertEquals(startDate, plan.getStartDate().toString());
+        assertEquals(endDate, plan.getEndDate().toString());
     }
 }
